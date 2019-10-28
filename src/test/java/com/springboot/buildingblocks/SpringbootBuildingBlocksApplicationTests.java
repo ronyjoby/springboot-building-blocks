d5214713d07cb341d9e50a7c42d46ce6ceae15d6
Rony Mathew <Rony.Mathew@harlandfs.com>
2019-10-28 14:38:37 +0530

    Initial

---
+++ src/test/java/com/springboot/buildingblocks/SpringbootBuildingBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.buildingblocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
